c00452a48c9a8958866e89239a9bb55c4b29d17b
Egesa Michael <praisewebhosts@gmail.com>
2022-06-22 00:04:20 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,53 @@
-<h3>Welcome User </h3>
+
+<html>
+<head>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<div class="container">
+
+
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#" style="padding-right: 320px; ">Internship Real Estate</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">My Bookings</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Logout</a>
+        </li>
+       
+    </div>
+  </div>
+</nav>
+
+
+<div class="row">
+    <?php
+    
+    for($count = 1; $count <= 8; $count++){
+?>
+<div class="col-md-3" style="padding:10px;">
+<img src="images/1.jpg" style="width:100%; height: 100px;">   
+<p style="font-weight: bold">Hello World</p> 
+<a href="#" class="btn btn-success">Book House</a>
+<br>
+</div>
+<?php
+    }
+    ?>
+</div>
+
+
